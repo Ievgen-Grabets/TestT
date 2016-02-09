96f8c06e65d7091ba309320e1e6bf68650945706
Ievgen-Grabets <didrash@mail.ru>
2016-02-09 18:09:29 +0200

    init branch

--- src/main/java/evg/testt/controller/DepartmentController.java
+++ src/main/java/evg/testt/controller/DepartmentController.java
@@ -18,6 +18,9 @@ import java.sql.SQLException;
 @Controller
 public class DepartmentController {
 
+    /**
+     * first init commit
+     */
     @Autowired
     DepartmentService departmentService;
 
